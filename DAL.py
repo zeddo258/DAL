5e4d0618fd4619cad9b647e77a3da148a3654b89
zeddo258 <94799753+zeddo258@users.noreply.github.com>
2023-09-28 19:20:21 +0800

    Add files via upload

---
+++ DAL.py
@@ -0,0 +1,84 @@
+import os
+import subprocess
+import difflib
+
+def getFileName(fileName, pathName):
+  for x in os.listdir(pathName):
+    fName = os.path.basename(x)
+    if ".c" in fName:
+      fileName.append(fName)
+
+def getDir():
+  return "code"
+
+
+def compileFile(fileName, pathName, execFileNames):
+  '''
+    This function is to compile the code into executable 
+  '''
+  wd = os.getcwd() # current working dir 
+  os.chdir(wd + '/code' ) # Change to the dir in which contain student code 
+  #To do: extract student ID from fileName
+  for file in fileName:
+    exe = file[0:file.find('_')]
+    std = subprocess.run(["g++", file, "-o" ,exe], capture_output=True)
+    if (os.path.exists(exe) == True ):
+      print("Compile status: ", exe, " success !!")
+      execFileNames.append(exe)
+    else: 
+      errLog = exe + "_errLog.txt" 
+      file = open(errLog, "w+")
+      file.write(std.stderr.decode())
+      file.close()
+      print("Compile status: ", exe, " failed, error available at :", errLog)   
+
+def executeFile(execFileNames, ansFileNames):
+  '''
+    This function take testing case number and exec file
+  '''
+  # Get command
+  for exe in execFileNames:
+    # Student answer file
+    ansFile = exe + "_ans.txt" 
+    ansFileNames.append(ansFile)
+    file = open(ansFile, "w+")
+    # Get command
+    commands = open("command.txt")
+    # Start executing
+    print("\nCurrently running :", exe)
+    exePath = "./" + exe # Exec command
+    subprocess.run([exePath], stdin=commands, stdout=file)
+    print("\nSuccessfully execute", exe)
+
+    file.close()
+    commands.close()
+
+  
+def compareFile(ansFileNames):
+  ans_path = "ans.txt"
+  for file in ansFileNames:
+    print("Checking student:", file)
+    student_ans = open(file, encoding="utf8").readlines()
+    correct_ans = open(ans_path).readlines()
+    diff = difflib.HtmlDiff().make_file(student_ans, correct_ans, ans_path, file)
+    report = file[0:file.find('_')] + '_res.html'
+    diff_report = open(report, 'w+')
+    diff_report.write(diff)
+    diff_report.close()
+
+
+def run():
+  fileName = []
+  pathName = getDir() 
+  execFileNames = []
+  ansFileNames = []
+  getFileName(fileName, pathName)
+  compileFile(fileName, pathName, execFileNames) 
+  executeFile(execFileNames,ansFileNames)
+  compareFile(ansFileNames)
+  
+
+  wd = os.getcwd()
+  os.chdir(wd + "/..")
+  
+
--- main.py
+++ main.py
@@ -1,24 +1,33 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_dropzone import Dropzone
-
+from DAL import run
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 app.config.update(
-    UPLOAD_PATH = os.path.join(basedir,'files'),
+    UPLOAD_PATH = os.path.join(basedir,'code'),
     DROPZONE_MAX_FILE_SIZE = 1024,
-    DROPZONE_TIMEOUT = 5*60*1000
+    DROPZONE_TIMEOUT = 5*60*1000,
+    DROPZONE_ALLOWED_FILE_CUSTOM = True,
+    DROPZONE_ALLOWED_FILE_TYPE = '.cpp, .c'
 )
 
 dropzone = Dropzone(app)
 @app.route('/', methods=['GET','POST'])
-
 def upload():
     if request.method == 'POST':
         file = request.files.get('file')
         file.save(os.path.join(app.config['UPLOAD_PATH'], file.filename) )
     return render_template('index.html')
+
+@app.route('/exec')
+def exec(): 
+    run()
+    #should return the list html compare
+    return "\nFinished."
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
