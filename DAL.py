ade346521aed8f9a51a86bcff7c70edb0a95b53f
jason89923 <jason89923@gmail.com>
2023-10-02 20:09:06 +0000

    update

--- DAL.py
+++ DAL.py
@@ -4,104 +4,95 @@ import difflib
 from zipfile import ZipFile, ZIP_DEFLATED
 
 
-def clear_prevAns():
-  dir = './ans'
-  for x in os.listdir(dir):
-    os.remove(os.path.join(dir, x))
-
-def getFileName(fileName, pathName):
-  if (os.path.exists(pathName) == False):
-    os.chdir("..")
-  for x in os.listdir(pathName):
-    fName = os.path.basename(x)
-    if ".c" in fName:
-      fileName.append(fName)
+def getFileName():
+    fileName = []
+    for x in os.listdir('.'):
+        fName = os.path.basename(x)
+        if ".c" in fName:
+            fileName.append(fName)
+    return fileName
 
 
-def zipFile():
-  zip_path = './ans.zip'
-  dir = './ans'
-  
-  with ZipFile(zip_path, 'w',ZIP_DEFLATED) as zip:
-    for file in os.listdir(dir):
-      fileDir = dir + '/' + file
-      zip.write(fileDir, arcname=file)
+def compileFile(fileName: list[str]):
+    '''
+      This function is to compile the code into executable 
+    '''
+    # To do: extract student ID from fileName
+    execFileNames = []
+    
+    
+    for file in fileName:
+        exe = file[0:file.rfind('.')]
+        std = subprocess.run(["g++", file, "-o", exe], capture_output=True)
+        if (os.path.exists(exe) == True):
+            print("Compile status: ", exe, " success !!")
+            execFileNames.append(exe)
+        else:
+            errLog = exe + "_errLog.txt"
+            file = open(errLog, "w+")
+            file.write(std.stderr.decode())
+            file.close()
+            print("Compile status: ", exe, " failed, error available at :", errLog)
+    return execFileNames
 
 
-def compileFile(fileName, pathName, execFileNames):
-  '''
-    This function is to compile the code into executable 
-  '''
-  os.chdir(pathName) # Change to the dir in which contain student code 
-  #To do: extract student ID from fileName
-  for file in fileName:
-    exe = file[0:file.find('_')]
-    std = subprocess.run(["g++", file, "-o" ,exe], capture_output=True)
-    if (os.path.exists(exe) == True ):
-      print("Compile status: ", exe, " success !!")
-      execFileNames.append(exe)
-    else: 
-      errLog = exe + "_errLog.txt" 
-      file = open(errLog, "w+")
-      file.write(std.stderr.decode())
-      file.close()
-      print("Compile status: ", exe, " failed, error available at :", errLog)   
-
-def executeFile(execFileNames, ansFileNames):
-  '''
-    This function take testing case number and exec file
-  '''
-  # Get command
-  for exe in execFileNames:
-    # Student answer file
-    ansFile = exe + "_ans.txt" 
-    ansFileNames.append(ansFile)
-    file = open(ansFile, "w+")
+def executeFile(execFileNames):
+    '''
+      This function take testing case number and exec file
+    '''
+    ansFileNames = []
     # Get command
-    commands = open("command.txt")
-    # Start executing
-    print("\nCurrently running :", exe)
-    exePath = "./" + exe # Exec command
-    subprocess.run([exePath], stdin=commands, stdout=file)
-    print("\nSuccessfully execute", exe)
+    for exe in execFileNames:
+        # Student answer file
+        ansFile = exe + "_ans.txt"
+        ansFileNames.append(ansFile)
+        file = open(ansFile, "w+")
+        # Get command
+        commands = open("command.txt")
+        # Start executing
+        print("\nCurrently running :", exe)
+        exePath = "./" + exe  # Exec command
+        subprocess.run([exePath], stdin=commands, stdout=file)
+        print("\nSuccessfully execute", exe)
 
-    file.close()
-    commands.close()
-
-  
-def compareFile(ansFileNames):
-  ans_path = "ans.txt"
-  for file in ansFileNames:
-    print("Checking student:", file)
-    # Opening file for comparision
-    student_ans = open(file, encoding="big5").readlines()
-    correct_ans = open(ans_path).readlines()
-    # Create html content 
-    # to display different
-    diff = difflib.HtmlDiff().make_file(student_ans, correct_ans, ans_path, file)
-
-    # Write html to file and save it 
-    report = file[0:file.find('_')] + '_res.html'
-    dir = "../ans/" + report
-    diff_report = open(dir, 'w+')
-    diff_report.write(diff)
-    diff_report.close()
+        file.close()
+        commands.close()
+    return ansFileNames
 
 
-def run(pathName):
-  clear_prevAns() 
+def compareFile(ansFileNames: list[str], folder_name: str):
+    ans_path = "ans.txt"
+    
+    report_list = []
+    for file in ansFileNames:
+        print("Checking student:", file)
+        # Opening file for comparision
+        student_ans = open(file, encoding="big5").readlines()
+        correct_ans = open(ans_path).readlines()
+        # Create html content
+        # to display different
+        diff = difflib.HtmlDiff().make_file(student_ans, correct_ans, ans_path, file)
 
-  fileName = []
-  execFileNames = []
-  ansFileNames = []
-  
-  getFileName(fileName, pathName)
-  compileFile(fileName, pathName, execFileNames) 
-  executeFile(execFileNames,ansFileNames)
-  compareFile(ansFileNames)
-  
-  os.chdir('..')
-  zipFile()
-   
-  
+        # Write html to file and save it
+        report_name = file[0:file.rfind('_')] + '_compare.html'
+        report_folder_path = "../../static/result/" + folder_name
+        if not os.path.exists(report_folder_path):
+            os.makedirs(report_folder_path)
+        with open(os.path.join(report_folder_path, report_name), "w") as file:
+            file.write(diff)
+        report_list.append(report_name)
+    return report_list
 
+
+def run(folder_name: str):
+  try:
+    folder_path = os.path.join('uploads', folder_name)
+    os.chdir(folder_path)
+
+    fileName = getFileName()
+    execFileNames = compileFile(fileName)
+    ansFileNames = executeFile(execFileNames)
+    result = compareFile(ansFileNames, folder_name)
+    return zip(fileName, result)
+  finally:
+    os.chdir('../..')
--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 import os
-from flask import Flask, render_template, request, send_file, url_for, redirect
+from flask import Flask, render_template, request, send_file, url_for, redirect, session, jsonify
 from flask_dropzone import Dropzone
+from configparser import ConfigParser
+import uuid
+import time
 from DAL import run
+
+
+current_user_folder = {}
+
+
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
@@ -12,9 +20,19 @@ app.config.update(
     DROPZONE_ALLOWED_FILE_TYPE = '.cpp, .c, .txt'
 )
 
+
+config = ConfigParser()
+config.read('config.ini')
+
+secret_key = config.get('Settings', 'SECRET_KEY')
+app.secret_key = secret_key
+
 dropzone = Dropzone(app)
 
 
+
+
+"""
 @app.route('/', methods=['GET','POST'])
 def home():
     if (request.method == 'POST'):
@@ -25,25 +43,47 @@ def home():
         return redirect(url_for("upload"))
     else:
         return render_template('index.html')
+"""
 
-@app.route('/upload', methods=['GET','POST'])
+def get_uid():
+    if 'user_id' not in session:
+        # 為新使用者生成一個唯一ID
+        session['user_id'] = str(uuid.uuid4())
+    user_id = session['user_id']
+    return user_id
+
+
+@app.route('/', methods=['GET','POST'])
 def upload():
+    user_id = get_uid()
+    
     if request.method == 'POST':
+        if user_id not in current_user_folder:
+            unique_path = str(int(time.time())) + '-' + str(uuid.uuid4())
+            current_user_folder[user_id] = unique_path
+        else:
+            unique_path = current_user_folder[user_id]
         file = request.files.get('file')
-        file.save(os.path.join(app.config['UPLOAD_PATH'], file.filename) )
+        save_path = os.path.join('uploads', unique_path)
+        if not os.path.exists(save_path):
+            os.makedirs(save_path)
+        file.save(os.path.join(save_path, file.filename))
     return render_template('upload.html')
-
-@app.route('/list_html')
-def list_htmls():
-    html_files = os.listdir("ans")
-    return render_template("list_html.html", html_files=html_files)
 
 @app.route('/exec')
 def exec():
-    path = app.config['UPLOAD_PATH']
-    res = run(path)
-    #should return the list html compare
-    return send_file('./ans.zip')
+    try:
+        user_id = get_uid()
+        folder_name = current_user_folder[user_id]
+        del current_user_folder[user_id]
+        result = run(folder_name)
+        html_content = render_template('report_list_template.html', result=result)
+        
+        with open(f"./static/result/{folder_name}/test_report.html", "w") as f:
+            f.write(html_content)
+        return jsonify({'report_filename': folder_name})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
 
 
